50f9245a84cda2ff0741d9a671a8a930c165c91a
Eddie <eddie.xu.nyc@gmail.com>
2020-12-11 20:45:56 -0500

    the map is live

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -1,35 +1,66 @@
 //earthquake URL
 var earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
 
+//magitude size marker function
+function markSize(data){
+  return data * 10;
+}
+
+//magnitude color marker function
+function markColor(data){
+  if(data > 5){
+    return 'red'
+  }
+  else if(data > 3){
+    return 'yellow'
+  }
+  else if(data > 1){
+    return 'blue'
+  }
+  else {
+    return 'green'
+  }
+}
+
 // Perform a GET request to the query URL
 d3.json(earthquakeUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
+
   createFeatures(data.features);
   console.log(data.features);
 });
 
 function createFeatures(earthquakeData) {
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
+  //Create a popup with earthquake info
+  function popUp(feature, layer){
     layer.bindPopup("<h3>Place: " + feature.properties.place + "</h3><h3>Magnitude: " + feature.properties.mag +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
+  //Create a marker function
+  function marker(feature, location){
+    var earthMark = {
+      stroke: false,
+      radius: markSize(feature.properties.mag),
+      color: markColor(feature.properties.mag)
+    }
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
+    return L.circleMarker(location, earthMark);
+
+  }
+
+  //Create a geoJson Layer
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+    onEachFeature: popUp,
+    pointToLayer: marker
   });
 
-  // Sending our earthquakes layer to the createMap function
+  //createMap function
   createMap(earthquakes);
 }
 
 function createMap(earthquakes) {
 
-  // Define streetmap and darkmap layers
+  // Create streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -46,18 +77,17 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  // Define a baseMaps object to hold our base layers
+  // Define a baseMaps and an overlay object
   var baseMaps = {
     "Street Map": streetmap,
     "Dark Map": darkmap
   };
 
-  // Create overlay object to hold our overlay layer
   var overlayMaps = {
     Earthquakes: earthquakes
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  // Create a map object
   var myMap = L.map("mapid", {
     center: [
       37.09, -95.71
@@ -65,6 +95,24 @@ function createMap(earthquakes) {
     zoom: 5,
     layers: [streetmap, earthquakes]
   });
+
+  // Create a legend
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+
+    // Add min & max
+    var legendInfo = "<h1>Magnitude Legend</h1>" +
+      "<table><tr><th> > 5</th><td>Red</td></tr>" +
+      "<tr><th> > 3</th><td>Yellow</td></tr>" + 
+      "<tr><th> > 1</th><td>Blue</td></tr>" +
+      "<tr><th> < 1</th><td>Green</td></tr></table>";
+
+    div.innerHTML = legendInfo;
+    return div;
+  };
+
+  legend.addTo(myMap);
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
